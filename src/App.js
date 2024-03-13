c8507113eb41a5ef4f5110f4a2a148a996200654
Talon Revis <beacon.web3@gmail.com>
2024-03-13 17:46:45 -0500

    Set up Routes in separate component and file, add import to app.js and referenced component at end of app, started component for order form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import {Routes, Route, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import RoutesComponent from "./Routes";
 
 
 const App = () => {
   const style = {border: '2px solid black', display: 'flex', alignItems: 'center'}
   const imgStyle = {border: '2px solid black', margin: '1rem .5rem', padding: '1rem 1rem'}
 
-  return (
-    
+  return ( 
     <><section id='Title' style={{ ...style, margin: '1rem', padding: '0 0.5rem', justifyContent: 'space-between' }}>
       <h1 style={{ ...style, border: '0' }}>Bloomtech Eats</h1>
       <nav id="navBar" style={{ ...style, border: '0' }}>
@@ -47,8 +47,8 @@ const App = () => {
             <img style={imgStyle} src="image1.jpg" alt="Image 1" />
           </div>
         </div>
+        <RoutesComponent />
       </section></>
-    
   );
 };
 export default App;
