8cc311a9473e7243e5a42bbfb186215cdbced783
Talon Revis <beacon.web3@gmail.com>
2024-03-12 21:43:09 -0500

    finished cosmetics on top two divs, moving on to body

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 
 const App = () => {
-  const style = { margin: '1rem', padding: '0.5rem', border: '2px solid black', display: 'flex', alignItems: 'center'}
+  const style = {border: '2px solid black', display: 'flex', alignItems: 'center'}
 
   return (
     <BrowserRouter>
-      <div id='Title' style={{...style, justifyContent: 'space-between'}}>
+      <div id='Title' style={{...style,  margin: '1rem', padding: '0 0.5rem', justifyContent: 'space-between'}}>
         <h1 style={{...style, border: '0'}}>Bloomtech Eats</h1>
           <nav id="navBar" style={{...style, border:'0'}}>
             <Link style={{...style, marginRight: '10px'}} to="/">Home</Link>&nbsp;
             <Link style={{...style}} to="">Help</Link>
           </nav>
       </div>  
-      <div id='main' style={{...style,  flexDirection: 'column', textAlign: 'center'}}>
+      <div id='main' style={{...style,   margin: '1rem', padding: '5rem 0', flexDirection: 'column', textAlign: 'center'}}>
         <h1 style={{...style, border: '0', marginTop: '5rem'}}>Your favorite food, delivered while coding</h1>
         <nav>
           <Link style={{...style}} to='/pizza'>Pizza</Link>
