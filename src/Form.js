1df467c4af2de098f36bf8ed88913875c2fe4215
Talon Revis <beacon.web3@gmail.com>
2024-03-13 20:49:56 -0500

    added form.js with a form with an id of 'pizza-form'

---
+++ src/Form.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from 'react-router-dom'
+
+
+
+export default function Pizza () {
+
+    return (
+      <div className="Form">
+        <form id="pizza-form">
+          <input />
+        </form>
+      </div>
+    )
+}
